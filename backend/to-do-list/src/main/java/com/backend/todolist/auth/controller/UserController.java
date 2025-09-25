535687e3f24d722f3d7cb477d1ee0121be5efde5
shimrin <139621909+shimrin23@users.noreply.github.com>
2025-09-26 02:12:29 +0530

    Final2

--- backend/to-do-list/src/main/java/com/backend/todolist/auth/controller/UserController.java
+++ backend/to-do-list/src/main/java/com/backend/todolist/auth/controller/UserController.java
@@ -5,44 +5,33 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.backend.todolist.auth.jwt.JwtTokenGenerator;
-import com.backend.todolist.auth.repository.UserRepository;
 import com.backend.todolist.auth.service.UserService;
-import com.backend.todolist.errorhandler.CustomException;
 
 @RestController
-public class UserController {	
+@RequestMapping("/api/auth") // 1. Define a base path for all endpoints in this controller
+public class UserController {
+
+	private final UserService userService;
+
+	// 2. Use constructor injection for dependencies - this is a best practice
 	@Autowired
-	AuthenticationManager authenticationManager;
-	
-	@Autowired
-    PasswordEncoder passwordEncoder;
-    
-	@Autowired
-    JwtTokenGenerator jwtTokenGenerator;
-    
-	@Autowired
-    UserRepository userRepository;
-	
-	@Autowired
-	UserService userService;
-    
-	@ResponseStatus(code = HttpStatus.OK)
-	@RequestMapping(value = "/api/auth/signin", method = RequestMethod.POST)
+	public UserController(UserService userService) {
+		this.userService = userService;
+	}
+
+	// 3. Use more specific mapping annotations like @PostMapping
+	// 4. The @ResponseStatus is redundant when returning a ResponseEntity
+	@PostMapping("/signin")
     public ResponseEntity<UserSigninResponse> signin(@Valid @RequestBody UserSigninRequest userSigninRequest) {
 		return new ResponseEntity<>(userService.signin(userSigninRequest), HttpStatus.OK);
     }
-	
-	@ResponseStatus(code = HttpStatus.OK)
-	@RequestMapping(value = "/api/auth/signup", method = RequestMethod.POST)
+
+	@PostMapping("/signup")
     public ResponseEntity<UserSignupResponse> signup(@Valid @RequestBody UserSignupRequest userSignupRequest) {
 		return new ResponseEntity<>(userService.signup(userSignupRequest), HttpStatus.OK);
     }
